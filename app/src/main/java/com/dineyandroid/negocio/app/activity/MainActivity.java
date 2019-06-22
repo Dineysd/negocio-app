fb11b7f0ad7f05d05970770213d0d4abecbee517
dineysd <dineysd@hotmail.com>
2019-06-22 19:14:57 -0300

    configuracao firebase

--- app/src/main/java/com/dineyandroid/negocio/app/activity/MainActivity.java
+++ app/src/main/java/com/dineyandroid/negocio/app/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dineyandroid.negocio.app.activity;
 
+import android.app.Application;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,15 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import com.dineyandroid.negocio.app.R;
+import com.dineyandroid.negocio.app.helper.ConfiguracaoFirebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class  MainActivity extends AppCompatActivity {
 
@@ -16,12 +27,17 @@ public class  MainActivity extends AppCompatActivity {
     private EditText campoEmail, campoSenha;
     private Switch tipoAcesso;
 
+    private FirebaseAuth autentificacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //FirebaseApp.initializeApp(MainActivity.this);
         inicializarComponentes();
+        autentificacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
         botaoAcessar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,31 +47,92 @@ public class  MainActivity extends AppCompatActivity {
                 if(!email.isEmpty()){
                     if (!senha.isEmpty()){
                         // Verificar estado do switch
-                        VerificarSwitch();
+                        verificarSwitch(email, senha);
 
                     } else {
-                        Toast.makeText(MainActivity.this, "Preencha a senha!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Preencha a senha!",
+                                Toast.LENGTH_SHORT).show();
                     }
 
                 } else {
-                    Toast.makeText(MainActivity.this, "Preencha E-mail!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Preencha E-mail!",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    private void VerificarSwitch() {
+    private void verificarSwitch(String email, String senha) {
         if(tipoAcesso.isChecked()){//cadastro
-
+            autentificacao.createUserWithEmailAndPassword(
+                    email, senha
+            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    cadastrar(task);
+                }
+            });
         }else{//Login
+            autentificacao.signInWithEmailAndPassword(
+                    email, senha
+            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    logar(task);
+                }
+            });
+        }
+    }
+
+    private void logar(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()){
+            Toast.makeText(MainActivity.this,
+                    "Logado com sucesso!",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+
+            Toast.makeText(MainActivity.this,
+                    "Erro ao fazer login: "+ task.getException(),
+                    Toast.LENGTH_SHORT).show();
 
         }
     }
 
+    private void cadastrar(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()){
+            Toast.makeText(MainActivity.this,
+                    "Cadastro realizado com sucesso!",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            String erroExcecao = "";
+            erroExcecao = erroExcecaoException(task);
+            Toast.makeText(MainActivity.this, "Erro: "+ erroExcecao,
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private String erroExcecaoException(@NonNull Task<AuthResult> task) {
+        String erroExcecao = "";
+        try {
+            throw task.getException();
+        }catch (FirebaseAuthWeakPasswordException e){
+            erroExcecao = "Digite uma senha mais forte!";
+        }catch (FirebaseAuthInvalidCredentialsException e){
+            erroExcecao = "Por favor, digite um e-mail valido!";
+        }catch (FirebaseAuthUserCollisionException e){
+            erroExcecao = "Esta conta já foi cadastrada!";
+        }catch (Exception e){
+            erroExcecao = "Ao cadastrar usuário: " + e.getMessage();
+            e.printStackTrace();
+        }
+        return erroExcecao;
+    }
+
     private  void inicializarComponentes(){
-        campoEmail = findViewById(R.id.editCadastroEmail);
-        campoSenha = findViewById(R.id.editCadastroSenha);
-        botaoAcessar = findViewById(R.id.buttonAcesso);
-        tipoAcesso = findViewById(R.id.switchAcesso);
+        campoEmail = (EditText) findViewById(R.id.editCadastroEmail);
+        campoSenha = (EditText) findViewById(R.id.editCadastroSenha);
+        botaoAcessar = (Button) findViewById(R.id.buttonAcesso);
+        tipoAcesso = (Switch) findViewById(R.id.switchAcesso);
     }
 }
---
+++ app/src/main/java/com/dineyandroid/negocio/app/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,34 @@
+package com.dineyandroid.negocio.app.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ConfiguracaoFirebase {
+    private static DatabaseReference referenceFirebase;
+    private static FirebaseAuth firebaseAutenticacao;
+    private static StorageReference storageReference;
+
+    public static DatabaseReference getReferenceFirebase() {
+        if (referenceFirebase == null){
+            referenceFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return referenceFirebase;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao() {
+        if (firebaseAutenticacao == null){
+            firebaseAutenticacao = FirebaseAuth.getInstance();
+        }
+        return firebaseAutenticacao;
+    }
+
+    public static StorageReference getFirebaseStorage() {
+        if (storageReference == null){
+            storageReference = FirebaseStorage.getInstance().getReference();
+        }
+        return storageReference;
+    }
+}
